de4a2817e09ebb348a643b33c9614324d89e7aae
Carla Buongiorno <carlab201103@gmail.com>
2021-07-20 04:33:38 +0000

    Add google maps api script, change activities.js to script.js, create initial map function

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function initMap() {
+  // Create the map.
+   const map = new google.maps.Map(document.getElementById('map'), {
+       zoom: 7,
+       center: { lat: 52.632469, lng: -1.689423 },
+   });
+
+}
