11e66007691dac63901023f23e8b873a2a12b159
Carla Buongiorno <carlab201103@gmail.com>
2021-07-21 17:02:17 +0000

    Trial function to connect index.html activities to activity sections on activities.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,39 @@
-window.onload=function(){
+//id from index.html
+var surfing = document.getElementById("surfing");
 
+// ids from activities.html
+let actA = document.getElementById("surfing-activity");
+let actB = document.getElementById("snorkeling-activity");
+let actC = document.getElementById("dolphin-activity");
+let actD = document.getElementById("kite-surfing-activity");
+let actE = document.getElementById("scuba-diving-activity");
+
+// when 1 activity is displayed, other activities should be hidden
+function hideActivitySurfing(actB, actC, actD, actE) {
+
+  actB.addClass('d-none');
+  actC.addClass('d-none');
+  actD.addClass('d-none');
+  actE.addClass('d-none');
 
 }
+
+// onclick #surfing, call function 
+surfing.addEventListener('click', hideActivities);
+
+
+//       document.getElementById("surfing-activity").style.display = "block"; //show
+//       document.getElementById("snorkeling-activity").style.display = "none"; //hide
+//       document.getElementById("dolphin-activity").style.display = "none";
+//       document.getElementById("kite-surfing-activity").style.display = "none";
+//       document.getElementById("scuba-diving-activity").style.display = "none";
+//     })
+
+
+// var snorkeling = document.getElementById("snorkeling");
+
+//   document.getElementById("snorkeling-activity").style.display = "block"; //show
+//   document.getElementById("surfing-activity").style.display = "none"; //hide
+//   document.getElementById("dolphin-activity").style.display = "none";
+//   document.getElementById("kite-surfing-activity").style.display = "none";
+//   document.getElementById("scuba-diving-activity").style.display = "none";
