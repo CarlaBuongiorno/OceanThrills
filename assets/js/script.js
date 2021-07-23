43cb2a92e4071a143dcfe4b69f433659e8c015be
Carla Buongiorno <carlab201103@gmail.com>
2021-07-23 17:10:00 +0000

    Use sessionStorage to get and set clicked activities from the homepage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,113 +5,137 @@ let dolSection = document.getElementById("dolphin-activity");
 let kiteSection = document.getElementById("kite-surfing-activity");
 let scubaSection = document.getElementById("scuba-diving-activity");
 
-
-document.querySelectorAll('.surf').forEach(item => {
-  item.addEventListener('click', (_a, _b, _c, _d) => {
-    displaySurf();
+// Home page clicks
+// credit -> https://developer.mozilla.org/en-US/docs/Web/API/Window/sessionStorage#basic_usage
+document.querySelectorAll(".home-surf").forEach(item => {
+  // Listen for click in the text field
+  item.addEventListener("click", function (event) {
+    // And save the results into the session storage object
+    let storedSurf = event.target.parentElement.classList[1];
+    console.log(storedSurf);
+    sessionStorage.setItem("autosave", storedSurf);
   });
 });
+// See if we have an autosave value
+// (this will only happen if the page is accidentally refreshed)
+if (sessionStorage.getItem("autosave")) {
+  // Restore the contents of the text field
+  storedSurf = sessionStorage.getItem("autosave");
+  displaySurf();
+}
 
-// function displaySurf() {
-//   surfSection.style.display = "block"; //show
-//   snorkSection.style.display = "none"; //hide
+// document.querySelectorAll(".home-snork").forEach(item => {
+//   item.addEventListener("click", function (event) {
+//     let storedSnork = event.target.parentElement.classList[1];
+//     sessionStorage.setItem("autosave", storedSnork);
+//   });
+// });
+// if (sessionStorage.getItem("autosave")) {
+//   storedSnork = sessionStorage.getItem("autosave");
+//   displaySnork();
+// }
+
+// document.querySelectorAll(".home-dol").forEach(item => {
+//   item.addEventListener("click", function (event) {
+//     let storedDol = event.target.parentElement.classList[1];
+//     sessionStorage.setItem("autosave", storedDol);
+//   });
+// });
+// if (sessionStorage.getItem("autosave")) {
+//   storedDol = sessionStorage.getItem("autosave");
+//   displayDol();
+// }
+
+// document.querySelectorAll(".home-kite").forEach(item => {
+//   item.addEventListener("click", function (event) {
+//     let storedKite = event.target.parentElement.classList[1];
+//     sessionStorage.setItem("autosave", storedKite);
+//   });
+// });
+// if (sessionStorage.getItem("autosave")) {
+//   storedKite = sessionStorage.getItem("autosave");
+//   displayKite();
+// }
+
+// document.querySelectorAll(".home-scuba").forEach(item => {
+//   item.addEventListener("click", function (event) {
+//     let storedScuba = event.target.parentElement.classList[1];
+//     sessionStorage.setItem("autosave", storedScuba);
+//   });
+// });
+// if (sessionStorage.getItem("autosave")) {
+//   storedScuba = sessionStorage.getItem("autosave");
+//   displayScuba();
+// }
+
+// Activity page clicks
+// document.querySelectorAll('.surf').forEach(item => {
+//   item.addEventListener('click', (_a, _b, _c, _d) => {
+//     displaySurf();
+//   });
+// });
+
+// document.querySelectorAll('.snork').forEach(item => {
+//   item.addEventListener('click', (_a, _b, _c, _d) => {
+//     displaySnork();
+//   });
+// });
+
+// document.querySelectorAll('.dol').forEach(item => {
+//   item.addEventListener('click', (_a, _b, _c, _d) => {
+//     displayDol();
+//   });
+// });
+
+// document.querySelectorAll('.kite').forEach(item => {
+//   item.addEventListener('click', (_a, _b, _c, _d) => {
+//     displayKite();
+//   });
+// });
+
+// document.querySelectorAll('.scuba').forEach(item => {
+//   item.addEventListener('click', (_a, _b, _c, _d) => {
+//     displayScuba();
+//   });
+// });
+
+// Functions
+function displaySurf() {
+  surfSection.style.display = "block"; //show
+  snorkSection.style.display = "none"; //hide
+  dolSection.style.display = "none";
+  kiteSection.style.display = "none";
+  scubaSection.style.display = "none";
+}
+
+// function displaySnork() {
+//   surfSection.style.display = "none";
+//   snorkSection.style.display = "block";
 //   dolSection.style.display = "none";
 //   kiteSection.style.display = "none";
 //   scubaSection.style.display = "none";
 // }
 
-document.querySelectorAll('.snork').forEach(item => {
-  item.addEventListener('click', (_a, _b, _c, _d) => {
-    surfSection.style.display = "none";
-    snorkSection.style.display = "block";
-    dolSection.style.display = "none";
-    kiteSection.style.display = "none";
-    scubaSection.style.display = "none";
-  });
-});
+// function displayDol() {
+//   surfSection.style.display = "none";
+//   snorkSection.style.display = "none";
+//   dolSection.style.display = "block";
+//   kiteSection.style.display = "none";
+//   scubaSection.style.display = "none";
+// }
 
-document.querySelectorAll('.dol').forEach(item => {
-  item.addEventListener('click', (_a, _b, _c, _d) => {
-    surfSection.style.display = "none";
-    snorkSection.style.display = "none";
-    dolSection.style.display = "block";
-    kiteSection.style.display = "none";
-    scubaSection.style.display = "none";
-  });
-});
+// function displayKite() {
+//   surfSection.style.display = "none";
+//   snorkSection.style.display = "none";
+//   dolSection.style.display = "none";
+//   kiteSection.style.display = "block";
+//   scubaSection.style.display = "none";
+// }
 
-document.querySelectorAll('.kite').forEach(item => {
-  item.addEventListener('click', (_a, _b, _c, _d) => {
-    surfSection.style.display = "none";
-    snorkSection.style.display = "none";
-    dolSection.style.display = "none";
-    kiteSection.style.display = "block";
-    scubaSection.style.display = "none";
-  });
-});
-
-document.querySelectorAll('.scuba').forEach(item => {
-  item.addEventListener('click', (_a, _b, _c, _d) => {
-    surfSection.style.display = "none";
-    snorkSection.style.display = "none";
-    dolSection.style.display = "none";
-    kiteSection.style.display = "none";
-    scubaSection.style.display = "block";
-  });
-});
-
-// ---------
-// let homeSurf = document.querySelectorAll(".home-surf").forEach(item => {
-//   item.addEventListener('click', (_a, _b, _c, _d) => {
-//     console.log('working');
-//     if (sessionStorage.setItem("autosave")) {
-//       // Restore the contents of the text field
-//       homeSurf.value = sessionStorage.setItem("autosave");
-//     }
-//     displaySurf();
-//   });
-// });
-
-
-document.querySelectorAll(".home-surf").forEach(item => {
-  // Listen for changes in the text field
-  item.addEventListener("change", function () {
-    // And save the results into the session storage object
-    // sessionStorage.setItem("autosave", item.value);
-    console.log(item.value);
-    console.log("working");
-  });
-  item.addEventListener('click', (_a, _b, _c, _d) => {
-    // displaySurf();
-    
-  });
-});
-
-
-let homeSnork = document.querySelectorAll(".home-snork").forEach(item => {
-  item.addEventListener('click', (whatShouldBeHere) => {
-    console.log('working');
-    sessionStorage.setItem("autosave", homeSnork.value);
-  });
-});
-
-let homeDol = document.querySelectorAll(".home-dol").forEach(item => {
-  item.addEventListener('click', (whatShouldBeHere) => {
-    console.log('working');
-    sessionStorage.setItem("autosave", homeDol.value);
-  });
-});
-
-let homeKite = document.querySelectorAll(".home-kite").forEach(item => {
-  item.addEventListener('click', (whatShouldBeHere) => {
-    console.log('working');
-    sessionStorage.setItem("autosave", homeKite.value);
-  });
-});
-
-let homeScuba = document.querySelectorAll(".home-scuba").forEach(item => {
-  item.addEventListener('click', (whatShouldBeHere) => {
-    console.log('working');
-    sessionStorage.setItem("autosave", homeScuba.value);
-  });
-});
+// function displayScuba() {
+//   surfSection.style.display = "none";
+//   snorkSection.style.display = "none";
+//   dolSection.style.display = "none";
+//   kiteSection.style.display = "none";
+//   scubaSection.style.display = "block";
+// }
